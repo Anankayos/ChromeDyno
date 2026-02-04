2eea14fa4cd38be073f48ae0962f46b02b49592e
Anankayos <alessandro.pesetti@gmail.com>
2026-02-04 14:32:57 +0100

    Art

--- ChromeDino/Assets/Scripts/Player.cs
+++ ChromeDino/Assets/Scripts/Player.cs
@@ -2,6 +2,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float maxHeight = 2f;
+    public Rigidbody2D rb;
+    public Vector2 jumpForce;
+    public bool isGrounded = true;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        {
+            Debug.Log("Player is grounded");
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        {
+            Debug.Log("Player is not grounded");
+            isGrounded = false;
+        }
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +29,18 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+       {
+           Debug.Log("Jump is " + maxHeight);
+           //rb.AddForce(jumpForce);
+           //rb.linearVelocity = jumpForce;
+           //cinematic formula = sqrt(2 * gravity * height)
+           rb.linearVelocity = Mathf.Sqrt(2f * maxHeight * Physics2D.gravity.magnitude * rb.gravityScale) * Vector2.up;
+
+       } 
+       else
+       {
+           Debug.Log("No Jump");
+       }
     }
 }

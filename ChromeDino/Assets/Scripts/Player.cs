cec3c1929fab44760c126ade77bff5efe1291f19
Anankayos <alessandro.pesetti@gmail.com>
2026-02-06 12:17:19 +0100

    Adding Sprite Sheet

--- ChromeDino/Assets/Scripts/Player.cs
+++ ChromeDino/Assets/Scripts/Player.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //VALUES
     public float maxHeight = 2f;
+    public float maxHeightInAir = 1f;
+    public bool isGrounded = true;
+    //REFERENCES 
     public Rigidbody2D rb;
     public Vector2 jumpForce;
-    public bool isGrounded = true;
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         {
@@ -20,11 +24,26 @@ public class Player : MonoBehaviour
             isGrounded = false;
         }
     }
+    // void Jump()
+    //{
+    //      rb.linearVelocity = Mathf.Sqrt(2f * maxHeight * Physics2D.gravity.magnitude * rb.gravityScale) * Vector2.up; 
+    //zz   }
+       void Jump(float height)
+       {
+          rb.linearVelocity = Mathf.Sqrt(2f * height * Physics2D.gravity.magnitude * rb.gravityScale) * Vector2.up; 
+       }
+       // Function that adds two integers and returns the result, no voidc
+    //   int Sum (int a , int b)
+    //    {
+    //       return a + b;
+    //    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        
-    }
+     {
+    //     Debug.Log(Sum(5, 3));
+    //     Debug.Log( Sum(2, 4));
+    //     Debug.Log(Sum(50, 30));   
+     }
 
     // Update is called once per frame
     void Update()
@@ -35,12 +54,21 @@ public class Player : MonoBehaviour
            //rb.AddForce(jumpForce);
            //rb.linearVelocity = jumpForce;
            //cinematic formula = sqrt(2 * gravity * height)
-           rb.linearVelocity = Mathf.Sqrt(2f * maxHeight * Physics2D.gravity.magnitude * rb.gravityScale) * Vector2.up;
-
+           Jump(maxHeight); 
        } 
        else
        {
            Debug.Log("No Jump");
        }
+    //    Air Jump
+    //    if (Input.GetKeyDown(KeyCode.Space) && !isGrounded)
+    //    {
+    //        Debug.Log("Jump is " + maxHeightInAir);
+    //        Jump(maxHeightInAir);
+    //    } 
+    //      else
+    //      {
+    //           Debug.Log("No Jump");
+    //      }
     }
 }
